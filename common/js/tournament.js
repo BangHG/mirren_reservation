8265c1628dcd71772ae8d46520b648f52d140990
BangHG <42599226+BangHG@users.noreply.github.com>
2024-05-30 19:03:01 +0900

    init

---
+++ common/js/tournament.js
@@ -0,0 +1,186 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+  startTournament();
+});
+
+function startTournament() {
+  const tournamentContainer = document.getElementById('tournament');
+
+  const items = [
+    { id: '1', name: '일라' },
+    { id: '2', name: '앰프사' },
+    { id: '3', name: '대니' },
+    { id: '4', name: '시슬리' },
+    { id: '5', name: '사라' },
+    { id: '6', name: '마키' },
+    { id: '7', name: '제랄디아' },
+    { id: '8', name: '크리슈나' },
+    { id: '9', name: '이카루스' },
+    { id: '10', name: '아넥스' },
+    { id: '11', name: '디아' },
+    { id: '12', name: '나르키소스' },
+    { id: '13', name: '아이카' },
+    { id: '14', name: '쿠로신' },
+    { id: '15', name: '플림턴' },
+    { id: '16', name: '야요이' },
+    { id: '17', name: '애슐' },
+    { id: '18', name: '아포피스' },
+    { id: '19', name: '나피' },
+    { id: '20', name: '쿠로아이' },
+    { id: '21', name: '베라' },
+    { id: '22', name: '캐롤린' },
+    { id: '23', name: '로잘리' },
+    { id: '24', name: '처칠' },
+    { id: '25', name: '나나브' },
+    { id: '26', name: '에어린' },
+    { id: '27', name: '베르다' },
+    { id: '28', name: '라일라' },
+    { id: '29', name: '플로라' },
+    { id: '30', name: '도로시' },
+    { id: '31', name: '호무라' },
+    { id: '32', name: '시아' },
+  ];
+
+  // 초기화 함수
+  function initializeTournament() {
+    tournamentContainer.innerHTML = '';
+    playRound(items);
+  }
+
+  // 32강 시작
+  function playRound(items) {
+    // 랜덤으로 섞기
+    items = shuffleArray(items);
+
+    if (items.length === 2) {
+      tournamentContainer.innerHTML = `<h2>결승전</h2>`;
+    } else {
+      tournamentContainer.innerHTML = `<h2>${items.length}강</h2>`;
+    }
+
+    let nextRound = [];
+
+    for (let i = 0; i < items.length; i += 2) {
+      const matchDiv = document.createElement('div');
+      matchDiv.className = 'match';
+
+      const item1 = items[i];
+      const item2 = items[i + 1];
+
+      const button1HTML = `
+        <button class="item-button">
+          <div class="img" style="background-image:url(../common/images/tournament/${item1.id}.webp)"> </div>
+          <p class="name">${item1.name}</p>
+        </button>`;
+      const button2HTML = `
+        <button class="item-button">
+          <div class="img" style="background-image:url(../common/images/tournament/${item2.id}.webp)"> </div>
+          <p class="name">${item2.name}</p>
+        </button>`;
+
+      const button1 = createElementFromHTML(button1HTML);
+      const button2 = createElementFromHTML(button2HTML);
+
+      button1.addEventListener('click', () => {
+        if (matchDiv.classList.contains('done')) return;
+        matchDiv.classList.add('done');
+        button2.disabled = true;
+        button1.classList.add('selected');
+        nextRound.push(item1);
+        checkRoundCompletion();
+        fadeOutMatch(matchDiv);
+      });
+
+      button2.addEventListener('click', () => {
+        if (matchDiv.classList.contains('done')) return;
+        matchDiv.classList.add('done');
+        button1.disabled = true;
+        button2.classList.add('selected');
+        nextRound.push(item2);
+        checkRoundCompletion();
+        fadeOutMatch(matchDiv);
+      });
+
+      matchDiv.appendChild(button1);
+      matchDiv.appendChild(createElementFromHTML('<span>vs</span> '));
+      matchDiv.appendChild(button2);
+
+      tournamentContainer.appendChild(matchDiv);
+    }
+
+    function checkRoundCompletion() {
+      if (nextRound.length === items.length / 2) {
+        if (nextRound.length === 1) {
+          setTimeout(() => {
+            tournamentContainer.style.transition = 'opacity 0.5s';
+            tournamentContainer.style.opacity = '0';
+
+            setTimeout(() => {
+              const winner = nextRound[0];
+              localStorage.setItem('tournamentWinner', JSON.stringify(winner));
+
+              tournamentContainer.innerHTML = `
+              <div class="winner">
+                <h2>세이비어님께서 첫눈에 반한 성흔은?</h2>
+                <div class="item-button">
+                  <div class="img" style="background-image:url(../common/images/tournament/${winner.id}.webp)"></div>
+                  <p class="name">${winner.name}</p>
+                </div>
+                <a class="btn download-btn" href="../common/images/tournament/${winner.id}.webp" download="${winner.name}.webp" target="_blank">다운로드</a>
+                <div class="share">
+                  <h3>공유하기</h3>
+                  <ul class="share__list"> <li><button>X</button></li> <li><button>카톡</button></li> <li><button>페북</button></li> </ul>
+                </div>
+                <button class="btn" id="restart-btn">다시하기</button>
+              </div>
+              `;
+
+              // 다시하기 버튼에 이벤트 추가
+              const restartButton = document.getElementById('restart-btn');
+              restartButton.addEventListener('click', initializeTournament);
+
+              tournamentContainer.style.opacity = '1';
+            }, 500);
+          }, 100);
+        } else {
+          setTimeout(() => {
+            tournamentContainer.style.transition = 'opacity 0.5s';
+            tournamentContainer.style.opacity = '0';
+            setTimeout(() => {
+              playRound(nextRound);
+              tournamentContainer.style.opacity = '1';
+            }, 500);
+          }, 100);
+        }
+      }
+    }
+  }
+
+  initializeTournament();
+}
+
+// HTML 문자열을 DOM 요소로 변환하는 함수
+function createElementFromHTML(htmlString) {
+  const div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
+}
+
+// 배열을 랜덤으로 섞기 위한 함수
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// 매치 페이드 아웃 함수
+function fadeOutMatch(matchDiv) {
+  setTimeout(() => {
+    matchDiv.style.transition = 'opacity 0.5s';
+    matchDiv.style.opacity = '0';
+    setTimeout(() => {
+      matchDiv.style.display = 'none';
+    }, 200);
+  }, 500); //0.5초간 정지
+}
---
+++ mobile/assets/js/reservation.js
@@ -0,0 +1,151 @@
+﻿//네비게이션
+
+function navClose() {
+  $('html').css({ overflowY: 'auto' });
+  $('.nav__toggle').stop().removeClass('active');
+  $('.nav__toggle').attr('title', '네비게이션 열기');
+  $('.header').stop().removeClass('active');
+}
+function navOpen() {
+  $('html').css({ overflowY: 'hidden' });
+  $('.nav__toggle').stop().addClass('active');
+  $('.nav__toggle').attr('title', '네비게이션 닫기');
+  $('.header').stop().addClass('active');
+}
+
+$('.nav__toggle').click(function () {
+  if ($(this).hasClass('active')) {
+    navClose();
+  } else {
+    navOpen();
+  }
+});
+
+//네비클릭시
+$('.nav .nav-list a.link').on('click', function () {
+  const href = $(this).attr('href');
+
+  // if (href && href.includes('#section')) {
+  const hrefOn = href.split('?')[0];
+  const active = href.split('?')[1];
+  // console.log(active);
+
+  gsap.to(window, { duration: 0.5, scrollTo: { y: hrefOn, offsetY: 30 } });
+  // gsap.to(window, { duration: 0.3, scrollTo: hrefOn });
+
+  if (href && href.includes('?media--')) {
+    // console.log(active);
+    $(`.section-media .tab-content`).removeClass('active');
+    $(`.section-media .tab-list .tab-link`).removeClass('active');
+
+    $(`.section-media .tab-list .tab-link[data-hash=${active}]`).addClass('active');
+    $('#' + active).addClass('active');
+  }
+  // }
+
+  navClose();
+});
+
+// # 스크롤 이벤트 정의
+$(document).scroll(function (e) {
+  // if ($(window).scrollTop() > 100) {
+  //   $('.scrollDown').stop().fadeOut(300);
+  // } else {
+  //   $('.scrollDown').stop().fadeIn(300);
+  // }
+
+  if ($(window).scrollTop() > 500) {
+    $('.btnTop').fadeIn(300);
+    $('.header').addClass('fixed');
+  } else {
+    $('.btnTop').fadeOut(300);
+    $('.header').removeClass('fixed');
+  }
+});
+
+$(document).ready(function () {
+  // ## aos
+  AOS.init({
+    once: true,
+  });
+});
+
+$(window).on('load', function () {
+  //## 전환효과
+  $('body').attr('data-loading', 'done');
+});
+
+//IOS 100VH지원 스크립트
+// function mobileVh() {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', vh + 'px');
+//   // console.log(vh);
+// }
+
+// $(window).on('load resize', function () {
+//   mobileVh();
+// });
+
+// 페이지 리사이즈처리
+window.addEventListener('resize', function () {
+  // AOS refresh
+  setTimeout(function () {
+    AOS.refresh();
+  }, 500);
+});
+//오디오 제어
+function audioCv() {
+  var audio = document.getElementById('cv_audio');
+  $('.cv-btn').on('click', function () {
+    $('.cv-btn').removeClass('active');
+    audio.pause();
+
+    // 갈아끼기
+    const resource = $(this).attr('data-audio');
+
+    $('#cv_audio').attr('src', `${$rootUrl}/common/audio/${resource}.wav`);
+
+    // console.log(resource);
+
+    $(this).addClass('active');
+
+    //재생
+    audio.play();
+  });
+  //재생이 완료되면 버튼 불끄기
+  audio.addEventListener('ended', function () {
+    audio.currentTime = 0;
+    $('.cv-btn').removeClass('active');
+  });
+}
+
+$(window).on('load', function () {
+  goToPortraitMode();
+});
+
+$(window).on('resize', function () {
+  goToPortraitMode();
+});
+
+function goToPortraitMode() {
+  const windowW = window.innerWidth;
+  const windowH = window.innerHeight;
+  if (windowH <= windowW) {
+    // console.log(`${windowW}, ${windowH} 넌 지금 가로다 `);
+    $('.goToPortraitMode').addClass('show');
+    setTimeout(function () {
+      $('.goToPortraitMode').removeClass('show');
+    }, 6000);
+  } else {
+    $('.goToPortraitMode').removeClass('show');
+  }
+  // if (windowH <= windowW) {
+  //   // console.log(`${windowW}, ${windowH} 넌 지금 가로다 `);
+  //   $('.goToPortraitMode').stop().fadeIn(300); //addClass('show');
+  //   setTimeout(function () {
+  //     $('.goToPortraitMode').stop().fadeOut(300); //removeClass('show');
+  //   }, 6000);
+  // } else {
+  //   $('.goToPortraitMode').stop().fadeOut(300); //removeClass('show');
+  // }
+}
